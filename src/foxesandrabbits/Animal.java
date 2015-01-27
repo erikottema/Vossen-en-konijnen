3f2210879c8e0e403f19450b87f27da04cdd5c28
Henkvanslochteren <t.hoeksem.a00@gmail.com>
2015-01-27 18:02:22 +0100

    Maartens shit goed

--- src/foxesandrabbits/Animal.java
+++ src/foxesandrabbits/Animal.java
@@ -1,125 +1,126 @@
 package foxesandrabbits;
 import java.util.List;
+import java.util.Random;
 
 /**
- * A class representing shared characteristics of animals.
- * 
- * @author David J. Barnes and Michael Kölling
- * @version 2011.07.31
- */
+* A class representing shared characteristics of animals.
+* 
+* @author David J. Barnes and Michael KÃ¶lling
+* @version 2011.07.31
+*/
 public abstract class Animal
 {
-    // Whether the animal is alive or not.
-    private boolean alive;
-    // The animal's field.
-    private Field field;
-    // The animal's position in the field.
-    private Location location;
-<<<<<<< HEAD
-<<<<<<< HEAD
-    // The age.
-    protected int age;
-=======
-    //M Age
-    protected int age;
-    
->>>>>>> origin/master
-=======
-    //M Age
-    protected int age;
-    
->>>>>>> origin/master
-    
-    /**
-     * Create a new animal at location in field.
-     * 
-     * @param field The field currently occupied.
-     * @param location The location within the field.
-     */
-    public Animal(Field field, Location location)
-    {
-        age = 0;
-    	alive = true;
-        this.field = field;
-        setLocation(location);
-    }
-    
-    //Setter voor de Age van Fox voor opgave 10.44
-    protected void setAge(int age)
-    {
-    
-    	this.age = age;
-    	
-    }
-  
+// Whether the animal is alive or not.
+private boolean alive;
+// The animal's field.
+private Field field;
+// The animal's position in the field.
+private Location location;
+//M Age
+protected int age;
+// A shared random number generator to control breeding.
+private static final Random rand = Randomizer.getRandom();
 
-    //Getter voor de Age van Fox voor opgave 10.44
-    protected int getAge()
-    {
-    	
-    	return this.age;
-    	
-    }
-    
-    
-    /**
-     * Make this animal act - that is: make it do
-     * whatever it wants/needs to do.
-     * @param newAnimals A list to receive newly born animals.
-     */
-    abstract public void act(List<Animal> newAnimals);
+/**
+* Create a new animal at location in field.
+* 
+* @param field The field currently occupied.
+* @param location The location within the field.
+*/
+public Animal(Field field, Location location)
+{
+alive = true;
+this.field = field;
+setLocation(location);
+}
 
-    /**
-     * Check whether the animal is alive or not.
-     * @return true if the animal is still alive.
-     */
-    protected boolean isAlive()
-    {
-        return alive;
-    }
+/**
+* Make this animal act - that is: make it do
+* whatever it wants/needs to do.
+* @param newAnimals A list to receive newly born animals.
+*/
+abstract public void act(List<Animal> newAnimals);
+//M Abstract voor breeding age, wordt ook gebruikt in Fox en Rabbit die de respectievelijke breeding ages returnt.
+abstract protected int getBreedingAge();
+//M Abstract voor Max age, zelfde principe als hierboven.
+abstract protected int getMaxAge();
+//M Abstract voor canbreed
+abstract protected boolean canBreed();
+//M Abstract getter breeding propability
+abstract protected double getBreedingPropability();
+//M Abstract voor litter size
+abstract protected int getMaxLitterSize();
 
-    /**
-     * Indicate that the animal is no longer alive.
-     * It is removed from the field.
-     */
-    protected void setDead()
-    {
-        alive = false;
-        if(location != null) {
-            field.clear(location);
-            location = null;
-            field = null;
-        }
-    }
+/**
+* Check whether the animal is alive or not.
+* @return true if the animal is still alive.
+*/
+protected boolean isAlive()
+{
+return alive;
+}
 
-    /**
-     * Return the animal's location.
-     * @return The animal's location.
-     */
-    protected Location getLocation()
-    {
-        return location;
-    }
-    
-    /**
-     * Place the animal at the new location in the given field.
-     * @param newLocation The animal's new location.
-     */
-    protected void setLocation(Location newLocation)
-    {
-        if(location != null) {
-            field.clear(location);
-        }
-        location = newLocation;
-        field.place(this, newLocation);
-    }
-    
-    /**
-     * Return the animal's field.
-     * @return The animal's field.
-     */
-    protected Field getField()
-    {
-        return field;
-    }
+/**
+* Indicate that the animal is no longer alive.
+* It is removed from the field.
+*/
+protected void setDead()
+{
+alive = false;
+if(location != null) {
+field.clear(location);
+location = null;
+field = null;
+}
+}
+
+/**
+* Return the animal's location.
+* @return The animal's location.
+*/
+protected Location getLocation()
+{
+return location;
+}
+
+/**
+* Place the animal at the new location in the given field.
+* @param newLocation The animal's new location.
+*/
+protected void setLocation(Location newLocation)
+{
+if(location != null) {
+field.clear(location);
+}
+location = newLocation;
+field.place(this, newLocation);
+}
+
+/**
+* Return the animal's field.
+* @return The animal's field.
+*/
+protected Field getField()
+{
+return field;
+}
+
+//M Increment age, uit fox en rabbit gehaald en hier gezet
+protected void incrementAge() {
+age++;
+if(age > getMaxAge()) {
+setDead();
+}
+}
+
+//M Breed, vanuit fox en rabbit
+protected int breed()
+{
+int births = 0;
+if(canBreed() && rand.nextDouble() <= getBreedingPropability()) {
+births = rand.nextInt(getMaxLitterSize()) + 1;
+}
+return births;
+}
+
 }
